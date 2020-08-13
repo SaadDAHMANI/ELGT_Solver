d660572883ef714fc3dd093b5fdf9bad0ad91277
SaadDAHMANI <TahatAtakor7@gmail.com>
2020-08-13 21:42:46 +0100

    ELGTSolver version test.
    Signed-off-by: SaadDAHMANI <TahatAtakor7@gmail.com>

--- ELGTSolverLib.Tests/ELGTSolverTests.cs
+++ ELGTSolverLib.Tests/ELGTSolverTests.cs
@@ -5,12 +5,19 @@ using ELGTSolverLib;
 namespace ELGTSolverLib.Tests
 {
     public class ELGTSolverTests
-    {
-        readonly ELGTSolver _tst = new ELGTSolver();
+    {   
+        //solver under tests.
+        readonly ELGTSolver solver_tst;
+
+        public ELGTSolverTests()
+        {
+            solver_tst=new ELGTSolver();
+        }
+
         [Fact]
         public void VersionTest()
         {
-             
+            Assert.Equal("1.0.0", solver_tst.Version);
         }
     }
 }
