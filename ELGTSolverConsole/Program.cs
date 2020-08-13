ca65585977d4ceec0da2162143c14d13e5569909
SaadDAHMANI <TahatAtakor7@gmail.com>
2020-08-13 20:50:38 +0100

    first commit

---
+++ ELGTSolverConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ELGTSolverConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ELGT_Solver started ...");
+        }
+    }
+}
